5ab375d748d205858da182598def2c6a0e787390
Arvind Dhaka <adhaka@3-C-10.42heilbronn.de>
2024-07-04 04:21:18 +0200

    did testing for ex00 and drafted ex01

---
+++ ex01/main.cpp
@@ -0,0 +1,50 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   main.cpp                                           :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: adhaka <adhaka@student.42.fr>              +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/02/06 08:37:15 by adhaka            #+#    #+#             */
+/*   Updated: 2024/07/03 06:30:34 by adhaka           ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "iter.hpp"
+#include <iostream>
+
+template <typename T> void printIt(T &a)
+{
+	std::cout << a << std::endl;
+}
+
+template <typename T> void addOne(T &a)
+{
+	++a;
+}
+
+int main(void)
+{
+	int tab[] = {0, 1, 2, 3, 4};
+	int len = sizeof(tab) / sizeof(tab[0]);
+	std::cout << "Orignale int array:" << std::endl;
+	::iter(tab, len, printIt);
+	std::cout << std::endl;
+
+	::iter(tab, len, addOne);
+	std::cout << "Array after addOne:" << std::endl;
+	::iter(tab, len, printIt);
+	std::cout << std::endl;
+
+	double tab3[] = {0.0, 1.7, 2.2, 3.3, 4.8};
+	len = sizeof(tab3) / sizeof(tab3[0]);
+	std::cout << "Orignale double array:" << std::endl;
+	::iter(tab3, len, printIt);
+	std::cout << std::endl;
+
+	::iter(tab3, len, addOne);
+	std::cout << "Array after addOne:" << std::endl;
+	::iter(tab3, len, printIt);
+	std::cout << std::endl;
+	return 0;
+}
